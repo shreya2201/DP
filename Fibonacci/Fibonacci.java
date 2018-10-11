55010b77054ba3babeefd51231fd5a2f1f7e914c
shreya2201 <shreyapohekar@gmail.com>
2018-10-11 15:43:51 +0530

    Udated the method to calculate the series sum

--- Fibonacci/Fibonacci.java
+++ Fibonacci/Fibonacci.java
@@ -1,4 +1,4 @@
-//Java program for Fibonacci Series using Space 
+//Java program for Fibonacci Series using Space and calculate the sum of all the numbers in the series
 import java.util.*;
 class fibonacci 
 { 
@@ -11,22 +11,47 @@ class fibonacci
     /* 0th and 1st number of the series are 0 and 1*/
     f[0] = 0; 
     f[1] = 1; 
+   
       
     for (i = 2; i <= n; i++) 
     { 
        /* Add the previous 2 numbers in the series 
          and store it */
         f[i] = f[i-1] + f[i-2]; 
+   
     } 
        
     return f[n]; 
     
     } 
+    static int sum(int n) {
+        
+        int f[] = new int[n+2]; // 1 extra to handle case, n = 0 
+    int i; 
+       
+    /* 0th and 1st number of the series are 0 and 1*/
+    f[0] = 0; 
+    f[1] = 1; 
+    int sum = f[0] + f[1];
+      
+    for (i = 2; i <= n; i++) 
+    { 
+       /* Add the previous 2 numbers in the series 
+         and store it */
+        f[i] = f[i-1] + f[i-2]; 
+        sum += f[i]; 
+    } 
+       
+    return f[n]; 
+    
+    } 
+        
   
     public static void main (String args[]) 
     { 
       Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(fib(n)); 
+        System.out.println("the sum is "+ sum(n)); 
     } 
 }
